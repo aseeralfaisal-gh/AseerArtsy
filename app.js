32cfb99ceda1a8c9349945441a90b80254dc6583
aseeralfaisal-gh <aseeralfaisal@gmail.com>
2021-01-16 14:13:56 +0600

    new visuala

--- app.js
+++ app.js
@@ -42,3 +42,58 @@ for (let i=0; i<container.length; i++){
     }
 }
 });
+
+// const welcomePage = document.getElementById('wel-page');
+// welcomePage.addEventListener('click', ()=>{
+//     window.scrollTo({top: 755, behavior: 'smooth'});
+// });
+const welDiv = document.getElementById('wel-page');
+const welcomePage = document.getElementById('arrow');
+welcomePage.addEventListener('click', ()=>{
+    window.scrollTo({top: 755, behavior: 'smooth'});
+});
+welcomePage.addEventListener('wheel', ()=>{
+    window.scrollTo({top: 755, behavior: 'smooth'});
+});
+welcomePage.addEventListener('scroll', ()=>{
+    window.scrollTo({top: 755, behavior: 'smooth'});
+});
+const arrow = document.getElementById('arrow');
+arrow.addEventListener('mouseover', () =>{
+    window.scrollTo({top: 755, behavior: 'smooth'});
+})
+
+// const ul = document.getElementById('navList');
+// const li = ul.querySelectorAll('li');
+// const h4 = document.querySelectorAll('h4.td');
+// h4.addEventListener('click', (e) =>{
+//     console.log(e);
+// }
+// )
+
+const fb = document.getElementById('fb');
+fb.addEventListener('click', () =>{
+    window.open('https://www.facebook.com/aseeralfaisalsaad');
+});
+const img = document.querySelectorAll('img');
+const btnOpenImg = document.getElementById('btnOpenImg');
+btnOpenImg.addEventListener('click', () =>{
+    window.open(expandImg.src);
+});
+const btnSave = document.querySelector('.btnSave');
+btnSave.addEventListener('click', () => {
+    saveAs(expandImg.src);
+});
+const home = document.querySelector('.home');
+home.addEventListener('click', () =>{
+    welcomePage.scrollIntoView();
+});
+const aboutSegment = document.querySelector('.aboutSegment');
+aboutSegment.addEventListener('click', () =>{
+    aboutSegment.style.display = 'none';
+});
+const about = document.querySelector('.about');
+aboutSegment.style.display = 'none';
+about.addEventListener('click', () =>{
+    aboutSegment.style.display = '';
+});
